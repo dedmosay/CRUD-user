9c226f415af04a6713fb3e012d6e6e348b29a733
ilinoa <ilinoa90@gmail.com>
2021-06-01 17:46:46 +0300

    add func for deploy

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const config = require('config');
+const path = require("path");
 const app = express();
 
-
-const PORT = config.get("port");
+const PORT = config.get("port") || 5000;
 
 const options = {
     useNewUrlParser: true,
@@ -13,14 +13,28 @@ const options = {
     useFindAndModify: false
 }
 
+app.use(express.json({ extended: true }));
+
+app.use("/api", require("./routes/user.routes"))
+
+
+if(process.env.NODE_ENV === "production") {
+    app.use("/", express.static(path.join(__dirname, '../app', 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../app', 'build', 'index.html'))
+    })
+}
+
 const uri = config.get("uri");
 const namedb = config.get("namedb");
-const path = config.get("path");
-
+const pathSrc = config.get("pathSrc");
+ 
 async function connect() {
     try {
         console.log("Connecting database...")
-        await mongoose.connect( `${uri}/${namedb}?authSource=${path}&ssl=false`, options);
+
+        await mongoose.connect( `${uri}/${namedb}?authSource=${pathSrc}&ssl=false`, options);
+ 
         app.listen(PORT, () => console.log(`http://localhost:${PORT} server started`));
     } catch (e) {
         console.log("Server error: ", e.message)
@@ -30,7 +44,5 @@ async function connect() {
 
 connect();
 
-app.use(express.json({ extended: true }));
 
-app.use("/api", require("./routes/user.routes"))
 
